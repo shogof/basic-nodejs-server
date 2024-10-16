c07dc56adc7c954501d63fe1e2bd13919d3c6d37
Shegofa <shogofadeveloper12@gmail.com>
2024-10-16 14:04:25 +0400

    Added error handling middleware for invalid requests

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const usersRoute = require("./routes/users");
+const productsRoute = require("./routes/products");
 
 const app = express();
 const PORT = 3000;
@@ -9,11 +10,26 @@ app.use(bodyParser.json());
 
 app.use(express.static("public"));
 
+app.use((req, res, next) => {
+  console.log(`${req.method} request made to: ${req.url}`);
+  next();
+});
+
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/public/index.html");
 });
 
 app.use("/users", usersRoute);
+app.use("/products", productsRoute);
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ error: "Something went wrong!" });
+});
+
+app.use((req, res) => {
+  res.status(404).json({ error: "Route not found" });
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
